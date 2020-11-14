91a2e079b4141f66e2de7bc982d6ba56a0605c19
Nikita Baranov <nikmacentosh@gmail.com>
2020-11-14 21:00:56 +0200

    Added routing to App.js. TODO: Home Screen & Product Screen

---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ProductScreen() {
+    return (
+        <div>
+            <div className="row">
+                <div className="col-2">
+
+                </div>
+                <div className="col-1">
+
+                </div>
+                
+            </div>
+        </div>
+    )
+}
